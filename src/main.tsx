98c0a96a286ddeb7d99f9e6b93796d46d862c488
FoxyGamingOff <matbb28@gmail.com>
2026-04-26 20:07:50 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./Appnow its goood";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
